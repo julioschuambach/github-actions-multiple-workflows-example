e42cad526d8f63e7d0d72679e0da637141799436
julioschuambach <julioschuambach.dev@gmail.com>
2024-05-25 16:09:06 -0300

    resource: created classlib project

---
+++ GithubActionsMultipleWorkflowsExample/GithubActionsMultipleWorkflowsExample.ClassLib/Example.cs
@@ -0,0 +1,12 @@
+﻿namespace GithubActionsMultipleWorkflowsExample.ClassLib
+{
+    public class Example
+    {
+        public Guid Id { get; private set; } = Guid.Empty;
+
+        public bool IsIdEmpty()
+        {
+            return Id == Guid.Empty && Id.ToString() == "00000000-0000-0000-0000-000000000000";
+        }
+    }
+}
